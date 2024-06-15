0e436c97fcb5b14bfd31fca7afd544295a680646
Neville Brailsford <neville_brailsford@hotmail.com>
2024-06-15 14:28:07 +0100

    Suspend auditing during initial load of data.

--- app.bank/src/main/java/applications/bank/storage/BankRead.java
+++ app.bank/src/main/java/applications/bank/storage/BankRead.java
@@ -18,6 +18,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import application.audit.AuditService;
 import application.definition.ApplicationConfiguration;
 import application.storage.AbstractLoadData;
 import application.storage.XMLErrorHandler;
@@ -37,7 +38,9 @@ public class BankRead extends AbstractLoadData {
 	public void readData() throws IOException {
 		LOGGER.entering(CLASS_NAME, "readData");
 		try (InputStream archive = new BufferedInputStream(new FileInputStream(fileName()))) {
+			AuditService.suspend();
 			readDataFrom(archive);
+			AuditService.resume();
 		} catch (Exception e) {
 			IOException exc = new IOException("BankRead: Exception occurred - " + e.getMessage(), e);
 			LOGGER.throwing(CLASS_NAME, "readData", exc);
